096d03bb3e02a6e63fa3caab2babe86a233f4caa
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-08-12 20:37:18 +0200

    ref(shopping-bsaket): simplify spec using a sketch :rocket:

--- src/shopping-basket.spec.ts
+++ src/shopping-basket.spec.ts
@@ -1,17 +1,12 @@
 import { Kentan } from '@kentan-official/core';
 
-import { ForProduct } from '../test/sketches/product.sketch';
-import { ShoppingBasket } from './shopping-basket';
+import { ForShoppingBasket } from '../test/sketches/shopping-basket.sketch';
 
 describe('Shopping Basket', () => {
   describe('When one product is removed', () => {
     it('should update the total price', () => {
-      const xBox = Kentan.sketch(ForProduct).model();
-      const playStation = Kentan.sketch(ForProduct).model({ id: 2 });
-      const shoppingBasket = new ShoppingBasket();
-
-      shoppingBasket.products = [xBox, playStation];
-      shoppingBasket.remove(xBox);
+      const shoppingBasket = Kentan.sketch(ForShoppingBasket).model();
+      shoppingBasket.remove(shoppingBasket.products[0]);
 
       expect(shoppingBasket.total).toBe(300);
     });
