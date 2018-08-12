a74408153364c3e600554f5e6454c6d10ee20fee
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-08-12 19:37:07 +0200

    product: provide demo test

---
+++ src/product.spec.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+describe('Product', () => {
+  describe('When a price is given', () => {
+    it('should yield the actual price', () => {
+      const givenPrice = 300;
+      
+      const product: Product = {
+        title: 'XBox One',
+        description: 'Gaming Console',
+        priceInDollars: givenPrice
+      };
+
+      expect(product.priceInDollars).toBe(300);
+    });
+  });
+});
---
+++ src/shopping-basket.ts
@@ -0,0 +1,6 @@
+import { Product } from './product';
+
+export class ShoppingBasket {
+  products: Product[];
+  total: number;
+}
