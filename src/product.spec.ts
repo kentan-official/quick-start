38ac57c1bd6c69939078f445e46e32761acb172f
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-08-16 13:07:54 +0200

    lab: play witch test cases

--- src/product.spec.ts
+++ src/product.spec.ts
@@ -1,6 +1,7 @@
 import { Kentan } from '@kentan-official/core';
 
 import { ForProduct } from '../test/sketches/product.sketch';
+import { Product } from './product';
 
 describe('Product', () => {
   describe('When a price is given', () => {
@@ -10,5 +11,22 @@ describe('Product', () => {
 
       expect(product.priceInDollars).toBe(givenPrice);
     });
+  });
+
+  describe('Cases', () => {
+    const products: Product[] = Kentan.sketch(ForProduct)
+      .take(2)
+      .models({
+        id: i => i + 1
+      });
+
+    describe.each(products)(
+      'When product a product is created',
+      (m: Product) => {
+        it(`${m.id} should have a price of ${m.priceInDollars}$ `, () => {
+          expect(m.priceInDollars).toBe(m.priceInDollars);
+        });
+      }
+    );
   });
 });
